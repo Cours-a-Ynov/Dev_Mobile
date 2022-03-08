f897dabd9365a2161c544322bc31dc2e158b629b
Karrwolf <maloloyerviaud@gmail.com>
2022-03-08 14:17:55 +0100

    First

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-export default 8;
+const input = document.querySelector("input");
+const pre = document.querySelector("pre");
 
-console.log("Hello");
+const readFile = (file) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader ();
+        reader.readAsText(file);
+
+        reader.addEventListener("load", () => {
+            resolve(reader.result);
+        });
+        reader.addEventListener("error", () => {
+            reject(reader.error);
+        });
+    });
+}
+
+input.addEventListener("change", () => {
+    let promises = [];
+    for (const file of input.files) {
+        promises.push(readFile(file));
+    }
+
+    Promise.all(promises).then((texts) => {
+        pre.textContent = texts.join ("\n\n#########################################\n\n")
+    }).catch((e) => console.error(e));
+});
